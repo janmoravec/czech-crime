d51c9940a968e934d383ba4fd891eb83f327e928
Jan Štol <honzastol@gmail.com>
2018-05-08 16:36:28 +0200

    fixed getApiAreaDetail and getApiCrimes

--- app/models/CrimeData.php
+++ app/models/CrimeData.php
@@ -277,7 +277,7 @@ class CrimeData extends BaseModel {
     }
     public function getApiAreaDetail( $code ) {
         
-        $sql = 'SELECT * FROM AreaLookup WHERE Code = ' .$code ;
+        $sql = 'SELECT * FROM AreaLookup WHERE Code = "' .$code. '"';
         return $this->db->query( $sql );
 
     }
@@ -415,7 +415,7 @@ class CrimeData extends BaseModel {
         $sql .= " FROM CrimeData, AreaLookup, TimeLookup WHERE";
 
         if( isset( $areaId ) ) {
-            $sql .=  ' FK_Area_Lookup = ' . $areaId;
+            $sql .=  ' FK_Area_Lookup = "' . $areaId . '"';
         }
 
         if( isset( $crimeTypes ) ) {
